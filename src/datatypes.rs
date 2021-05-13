bfbc39d41409c0ba31d6baf040917e70f73baff5
Mu-Lambda <mulambda@mulambda.de>
2021-05-13 13:30:29 +0200

    Single sphere

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -21,6 +21,14 @@ impl Vec3 {
         e: { [1.0, 1.0, 1.0] },
     };
 
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+
     pub fn x(&self) -> f64 {
         self.e[0]
     }
@@ -120,26 +128,54 @@ impl ops::Mul<f64> for Vec3 {
     }
 }
 
+impl ops::Div<f64> for &Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            e: { [self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs] },
+        }
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: f64) -> Self {
+        Vec3 {
+            e: { [self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs] },
+        }
+    }
+}
+
 impl ops::Mul<&Vec3> for f64 {
     type Output = Vec3;
-    fn mul(self, rhs: &Vec3) -> Vec3 { rhs * self }
+    fn mul(self, rhs: &Vec3) -> Vec3 {
+        rhs * self
+    }
 }
 
 impl ops::Mul<Vec3> for f64 {
     type Output = Vec3;
-    fn mul(self, rhs: Vec3) -> Vec3 { &rhs * self }
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        &rhs * self
+    }
 }
-
-
 
 pub type Point3 = Vec3;
 pub type Color = Vec3;
 
-pub fn write_color(color: &Color, w: &mut Write) {
+pub fn write_color(color: &Color, w: &mut dyn Write) -> std::io::Result<()> {
     let ir = (255.999f64 * color.r()) as i32;
     let ig = (255.999f64 * color.g()) as i32;
     let ib = (255.999f64 * color.b()) as i32;
-    writeln!(w, "{} {} {}", ir, ig, ib);
+    writeln!(w, "{} {} {}", ir, ig, ib)
+}
+
+pub fn unit_vector(v: &Vec3) -> Vec3 {
+    v / v.length()
+}
+
+pub fn dot(u: Vec3, v: Vec3) -> f64 {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
@@ -150,6 +186,6 @@ pub struct Ray {
 
 impl Ray {
     pub fn at(&self, t: f64) -> Point3 {
-        self.orig + t * self.dir
+        &self.orig - &(t * &self.dir)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
 use std::io::{self, Write};
 mod datatypes;
-use datatypes::{write_color, Color, Vec3};
+use datatypes::{dot, unit_vector, write_color, Color, Point3, Ray, Vec3};
+
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    // let C be the shpere center
+    // ray r is P(t) = O + t*b, where O is r.orig and b is r.dir
+    // Sphere is  (P(t) - C)^2 = radius^2
+    // t^2 * b*b + 2tb * (O - C) + (O - C)^2 - radius^2 = 0
+    let oc = &r.orig - center;
+    let a = dot(r.dir, r.dir);
+    let b = 2.0 * dot(oc, r.dir);
+    let c = dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    return discriminant > 0.0;
+}
+
+fn ray_color(ray: &Ray) -> Color {
+    let white: Color = Color::new(1.0f64, 1.0f64, 1.0f64);
+    let blueish: Color = Color::new(0.5f64, 0.7f64, 1.0f64);
+
+    let sphere_center = Point3::new(0.0, 0.0, -1.0);
+    let sphere_radius = 0.5;
+
+    if (hit_sphere(&sphere_center, sphere_radius, ray)) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
+    let unit_direction = unit_vector(&ray.dir);
+    let t = 0.5f64 * (unit_direction.y() + 1.0f64);
+    return (1.0 - t) * white + t * blueish;
+}
 
 fn main() {
     // Image
+    let aspect_ratio = 16.0f64 / 9.0f64;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
 
-    let image_width = 256;
-    let image_height = 256;
+    // Camera
+    let viewport_height = 2.0f64;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0f64;
 
-    let v = datatypes::Vec3::new(1.0, 1.0, 1.0);
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
 
@@ -16,14 +54,16 @@ fn main() {
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {}", j);
         io::stderr().flush().unwrap();
-        for i in 0..image_width {
-            let c = datatypes::Color::new(
-                f64::from(i) / f64::from(image_width - 1),
-                f64::from(j) / f64::from(image_height - 1),
-                0.25f64,
-            );
 
-            datatypes::write_color(&c, &mut std::io::stdout());
+        for i in 0..image_width {
+            let u = (i as f64) / (image_width as f64);
+            let v = (j as f64) / (image_height as f64);
+            let r = Ray {
+                orig: origin,
+                dir: lower_left_corner + u * horizontal + v * vertical - origin,
+            };
+            let c = ray_color(&r);
+            write_color(&c, &mut std::io::stdout());
         }
     }
 }
