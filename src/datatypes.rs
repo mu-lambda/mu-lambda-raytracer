1680f8210fe655836c304176618a71cf2271b9d9
Mu-Lambda <mulambda@mulambda.de>
2021-05-13 11:06:09 +0200

    Moar commit

---
+++ src/datatypes.rs
@@ -0,0 +1,155 @@
+use std::io::Write;
+use std::ops;
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Vec3 {
+    pub e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+        Vec3 {
+            e: { [e0, e1, e2] },
+        }
+    }
+
+    pub const ZERO: Vec3 = Vec3 {
+        e: { [0.0, 0.0, 0.0] },
+    };
+
+    pub const ONE: Vec3 = Vec3 {
+        e: { [1.0, 1.0, 1.0] },
+    };
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
+
+    pub fn r(&self) -> f64 {
+        self.e[0]
+    }
+    pub fn g(&self) -> f64 {
+        self.e[1]
+    }
+    pub fn b(&self) -> f64 {
+        self.e[2]
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Vec3 {
+            e: {
+                [
+                    self.e[0] + rhs.e[0],
+                    self.e[1] + rhs.e[1],
+                    self.e[2] + rhs.e[2],
+                ]
+            },
+        }
+    }
+}
+
+impl ops::Add for &Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs: Self) -> Vec3 {
+        Vec3 {
+            e: {
+                [
+                    self.e[0] + rhs.e[0],
+                    self.e[1] + rhs.e[1],
+                    self.e[2] + rhs.e[2],
+                ]
+            },
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        Vec3 {
+            e: {
+                [
+                    self.e[0] - rhs.e[0],
+                    self.e[1] - rhs.e[1],
+                    self.e[2] - rhs.e[2],
+                ]
+            },
+        }
+    }
+}
+
+impl ops::Sub for &Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: Self) -> Vec3 {
+        Vec3 {
+            e: {
+                [
+                    self.e[0] - rhs.e[0],
+                    self.e[1] - rhs.e[1],
+                    self.e[2] - rhs.e[2],
+                ]
+            },
+        }
+    }
+}
+
+impl ops::Mul<f64> for &Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            e: { [self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs] },
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self {
+        Vec3 {
+            e: { [self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs] },
+        }
+    }
+}
+
+impl ops::Mul<&Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: &Vec3) -> Vec3 { rhs * self }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 { &rhs * self }
+}
+
+
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+
+pub fn write_color(color: &Color, w: &mut Write) {
+    let ir = (255.999f64 * color.r()) as i32;
+    let ig = (255.999f64 * color.g()) as i32;
+    let ib = (255.999f64 * color.b()) as i32;
+    writeln!(w, "{} {} {}", ir, ig, ib);
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Ray {
+    pub orig: Point3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + t * self.dir
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
 use std::io::{self, Write};
+mod datatypes;
+use datatypes::{write_color, Color, Vec3};
 
 fn main() {
-
     // Image
 
     let image_width = 256;
     let image_height = 256;
 
+    let v = datatypes::Vec3::new(1.0, 1.0, 1.0);
 
     // Render
-    
+
     println!("P3\n{} {}\n255", image_width, image_height);
     for j in (0..image_height).rev() {
-        eprint!("\rScanlines remaining: {}", j); io::stderr().flush().unwrap();
+        eprint!("\rScanlines remaining: {}", j);
+        io::stderr().flush().unwrap();
         for i in 0..image_width {
-            let r = f64::from(i) / f64::from(image_width - 1);
-            let g = f64::from(j) / f64::from(image_height - 1);
-            let b = 0.25f64;
+            let c = datatypes::Color::new(
+                f64::from(i) / f64::from(image_width - 1),
+                f64::from(j) / f64::from(image_height - 1),
+                0.25f64,
+            );
 
-            let ir = (255.999f64 * r) as i32;
-            let ig = (255.999f64 * g) as i32;
-            let ib = (255.999f64 * b) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
-
+            datatypes::write_color(&c, &mut std::io::stdout());
         }
     }
 }
-
