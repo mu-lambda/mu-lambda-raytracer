db8779114b5144dc324c2fb700c59c09c5dd8853
Mu-Lambda <mulambda@mulambda.de>
2021-05-24 21:01:48 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,16 @@ pub mod raytrace;
 pub mod rngator;
 pub mod shapes;
 pub mod vec;
+pub mod worlds;
 
 use camera::Camera;
 use clap::{App, Arg, ArgMatches};
 use hittable::Hittable;
-use materials::{Dielectric, Lambertian, Metal};
-use rand::Rng;
 use raytrace::RayTracer;
 use rngator::Rngator;
-use shapes::Sphere;
 use std::sync::atomic::{self, AtomicIsize, AtomicUsize};
 use std::time::Instant;
-use vec::{Color, Point3, Vec3};
+use vec::{Point3, Vec3};
 
 struct Parameters {
     pub random_world: bool,
@@ -106,71 +104,6 @@ fn args() -> Parameters {
     }
 }
 
-fn simple_world<'a>() -> Box<dyn Hittable + 'a> {
-    let mat_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
-    let mat_center = Lambertian::new(Color::new(0.1, 0.3, 0.5));
-    let mat_left = Dielectric::new(1.5);
-    let mat_right = Metal::new(Color::new(0.8, 0.6, 0.2), 0.0);
-
-    let mut world = bhv::SceneBuilder::new();
-
-    world
-        .add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground))
-        .add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center))
-        .add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left.clone()))
-        .add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.4, mat_left))
-        .add(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right));
-
-    let bhv = bhv::BHV::new(&mut world);
-    Box::new(bhv)
-}
-
-fn rnd01(rng: &mut dyn rand::RngCore) -> f64 {
-    rng.gen_range(0.0..1.0)
-}
-
-fn random_world<'a>(rng: &mut dyn rand::RngCore) -> Box<dyn Hittable + 'a> {
-    let mut world = bhv::SceneBuilder::new();
-
-    let ground_material = Lambertian::new(Color::new(0.5, 0.5, 0.5));
-    world.add(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material));
-
-    for a in -11..11 {
-        for b in -11..11 {
-            let choose_mat = rnd01(rng);
-            let center = Point3::new(a as f64 + 0.9 * rnd01(rng), 0.2, b as f64 + 0.9 * rnd01(rng));
-
-            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                if choose_mat < 0.8 {
-                    let albedo = Color::random_unit(rng) * Color::random_unit(rng);
-                    world.add(Sphere::new(center, 0.2, Lambertian::new(albedo)));
-                } else if choose_mat < 0.95 {
-                    let albedo = Color::random(0.5, 1.0, rng);
-                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
-                    world.add(Sphere::new(center, 0.2, Metal::new(albedo, fuzz)));
-                } else {
-                    world.add(Sphere::new(center, 0.2, Dielectric::new(1.5)));
-                }
-            }
-        }
-    }
-
-    world
-        .add(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, Dielectric::new(1.5)))
-        .add(Sphere::new(
-            Point3::new(-4.0, 1.0, 0.0),
-            1.0,
-            Lambertian::new(Color::new(0.4, 0.2, 0.1)),
-        ))
-        .add(Sphere::new(
-            Point3::new(4.0, 1.0, 0.0),
-            1.0,
-            Metal::new(Color::new(0.7, 0.6, 0.5), 0.0),
-        ));
-
-    Box::new(bhv::BHV::new(&mut world))
-}
-
 fn do_it<T>(parameters: Parameters, rngator: T)
 where
     T: Rngator,
@@ -180,7 +113,7 @@ where
 
     // World
     let world: Box<dyn Hittable> =
-        if parameters.random_world { random_world(rng) } else { simple_world() };
+        if parameters.random_world { worlds::random_world(rng) } else { worlds::simple_world() };
 
     // Camera
     let cam = Camera::new(
---
+++ src/worlds.rs
@@ -0,0 +1,71 @@
+use crate::bhv;
+use crate::hittable::Hittable;
+use crate::materials::{Dielectric, Lambertian, Metal};
+use crate::shapes::Sphere;
+use crate::vec::{Color, Point3};
+use rand::Rng;
+
+pub fn simple_world<'a>() -> Box<dyn Hittable + 'a> {
+    let mat_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
+    let mat_center = Lambertian::new(Color::new(0.1, 0.3, 0.5));
+    let mat_left = Dielectric::new(1.5);
+    let mat_right = Metal::new(Color::new(0.8, 0.6, 0.2), 0.0);
+
+    let mut world = bhv::SceneBuilder::new();
+
+    world
+        .add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground))
+        .add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center))
+        .add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left.clone()))
+        .add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.4, mat_left))
+        .add(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right));
+
+    let bhv = bhv::BHV::new(&mut world);
+    Box::new(bhv)
+}
+
+fn rnd01(rng: &mut dyn rand::RngCore) -> f64 {
+    rng.gen_range(0.0..1.0)
+}
+
+pub fn random_world<'a>(rng: &mut dyn rand::RngCore) -> Box<dyn Hittable + 'a> {
+    let mut world = bhv::SceneBuilder::new();
+
+    let ground_material = Lambertian::new(Color::new(0.5, 0.5, 0.5));
+    world.add(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rnd01(rng);
+            let center = Point3::new(a as f64 + 0.9 * rnd01(rng), 0.2, b as f64 + 0.9 * rnd01(rng));
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    let albedo = Color::random_unit(rng) * Color::random_unit(rng);
+                    world.add(Sphere::new(center, 0.2, Lambertian::new(albedo)));
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random(0.5, 1.0, rng);
+                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
+                    world.add(Sphere::new(center, 0.2, Metal::new(albedo, fuzz)));
+                } else {
+                    world.add(Sphere::new(center, 0.2, Dielectric::new(1.5)));
+                }
+            }
+        }
+    }
+
+    world
+        .add(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, Dielectric::new(1.5)))
+        .add(Sphere::new(
+            Point3::new(-4.0, 1.0, 0.0),
+            1.0,
+            Lambertian::new(Color::new(0.4, 0.2, 0.1)),
+        ))
+        .add(Sphere::new(
+            Point3::new(4.0, 1.0, 0.0),
+            1.0,
+            Metal::new(Color::new(0.7, 0.6, 0.5), 0.0),
+        ));
+
+    Box::new(bhv::BHV::new(&mut world))
+}
