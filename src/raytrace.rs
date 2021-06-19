6f8cf023d589bfa2560c2a2e069be0c23f85b34e
Mu-Lambda <mulambda@mulambda.de>
2021-06-19 13:03:12 +0200

    Minor fix.

--- src/raytrace.rs
+++ src/raytrace.rs
@@ -120,9 +120,10 @@ impl RayTracingAlgorithm for SingleLightSourceRayTracer {
                     let l = (self.light_source - hit.p).unit();
                     let v = -ray.dir.unit();
                     let h = (l + v).unit();
-                    let lamb = attenuation * self.intensity * l.dot(hit.normal).max(0.0);
+                    // All materials are Lambertian.
+                    let lambertian = attenuation * self.intensity * l.dot(hit.normal).max(0.0);
                     let blinn_phong = 0.5 * Color::ONE * self.intensity * h.dot(hit.normal).max(0.0).powi(100);
-                    return lamb + blinn_phong;
+                    return lambertian + blinn_phong;
                 }
                 None => {
                     return hit.material.emit(hit.u, hit.v, hit.p);
